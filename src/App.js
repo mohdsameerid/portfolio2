8d5f5e2113d76ce413130bcb2209804e6535bc6d
mohdsameerid <mohdsameeredu@gmail.com>
2023-02-10 15:38:09 -0800

    added skills contents

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import { Navbar  } from "./components/NavBar.js";
 import { Banner } from "./components/Banner";
 import Navbar from "./components/Navbar"
+import { Skills } from './components/skills';
 
 function App() {
   return (
     <div className='app'>
       <Navbar />
       <Banner />
+      <Skills />
     </div>
   );
 }
