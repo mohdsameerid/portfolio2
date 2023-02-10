e3e0f96cf299e4ca1df5af580994eeb9b907464f
mohdsameerid <mohdsameeredu@gmail.com>
2023-02-10 15:08:07 -0800

    added scroll styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,18 +3,33 @@ import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
-import { useState } from "react";
+import { useState , useEffect } from "react";
 
 const Navbars = () => {
 
   const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
 
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
   } 
 
   return(
-    <Navbar expand="md">
+    <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="/">
           <img src={logo} alt="Logo" />
