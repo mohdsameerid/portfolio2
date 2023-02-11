ecb5e81d89bdf6c1657f7c1226d2fe215aaa1a4f
mohdsameerid <mohdsameeredu@gmail.com>
2023-02-10 16:00:34 -0800

    added contact form

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Banner } from "./components/Banner";
 import Navbar from "./components/Navbar"
 import { Skills } from './components/skills';
-import { Projects } from "./components/Projects"
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Contact />
     </div>
   );
 }
